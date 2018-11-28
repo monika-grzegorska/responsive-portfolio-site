5abbabfae6bc02aaa3a3aef799cc051e4f322339
Monika Grzegorska <monika@grzegorska.pro>
2018-11-28 19:55:26 +0100

    Added practicles-JS to top section

--- js/main.js
+++ js/main.js
@@ -8,117 +8,117 @@ $(document).ready(function () {
     }
     //stars
 
-    //particlesJS("particles-JS", {
-    //    "particles": {
-    //        "number": {
-    //            "value": 700,
-    //            "density": {
-    //                "enable": true,
-    //                "value_area": 789.1476416322727
-    //            }
-    //        },
-    //        "color": {
-    //            "value": "#5bc0de"
-    //        },
-    //        "shape": {
-    //            "type": "circle",
-    //            "stroke": {
-    //                "width": 0,
-    //                "color": "#000000"
-    //            },
-    //            "polygon": {
-    //                "nb_sides": 5
-    //            },
-    //            "image": {
-    //                "src": "img/github.svg",
-    //                "width": 150,
-    //                "height": 150
-    //            }
-    //        },
-    //        "opacity": {
-    //            "value": 0.48927153781200905,
-    //            "random": false,
-    //            "anim": {
-    //                "enable": true,
-    //                "speed": 0.7,
-    //                "opacity_min": 0.1,
-    //                "sync": false
-    //            }
-    //        },
-    //        "size": {
-    //            "value": 4,
-    //            "random": true,
-    //            "anim": {
-    //                "enable": true,
-    //                "speed": 3,
-    //                "size_min": 0,
-    //                "sync": false
-    //            }
-    //        },
-    //        "line_linked": {
-    //            "enable": false,
-    //            "distance": 150,
-    //            "color": "#ffffff",
-    //            "opacity": 0.4,
-    //            "width": 1
-    //        },
-    //        "move": {
-    //            "enable": true,
-    //            "speed": 0.3,
-    //            "direction": "none",
-    //            "random": true,
-    //            "straight": false,
-    //            "out_mode": "out",
-    //            "bounce": false,
-    //            "attract": {
-    //                "enable": false,
-    //                "rotateX": 600,
-    //                "rotateY": 1200
-    //            }
-    //        }
-    //    },
-    //    "interactivity": {
-    //        "detect_on": "div",
-    //        "events": {
-    //            "onhover": {
-    //                "enable": true,
-    //                "mode": "bubble"
-    //            },
-    //            "onclick": {
-    //                "enable": true,
-    //                "mode": "push"
-    //            },
-    //            "resize": true
-    //        },
-    //        "modes": {
-    //            "grab": {
-    //                "distance": 400,
-    //                "line_linked": {
-    //                    "opacity": 1
-    //                }
-    //            },
-    //            "bubble": {
-    //                "distance": 83.91608391608392,
-    //                "size": 1,
-    //                "duration": 3,
-    //                "opacity": 1,
-    //                "speed": 3
-    //            },
-    //            "repulse": {
-    //                "distance": 200,
-    //                "duration": 0.4
-    //            },
-    //            "push": {
-    //                "particles_nb": 4
-    //            },
-    //            "remove": {
-    //                "particles_nb": 2
-    //            }
-    //        }
-    //    },
-    //    "retina_detect": true,
-    //    "interactive": true
-    //});
+    particlesJS("particles-JS", {
+        "particles": {
+            "number": {
+                "value": 700,
+                "density": {
+                    "enable": true,
+                    "value_area": 789.1476416322727
+                }
+            },
+            "color": {
+                "value": "#5bc0de"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 150,
+                    "height": 150
+                }
+            },
+            "opacity": {
+                "value": 0.48927153781200905,
+                "random": false,
+                "anim": {
+                    "enable": true,
+                    "speed": 0.7,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 4,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 3,
+                    "size_min": 0,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 0.3,
+                "direction": "none",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "div",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "bubble"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 83.91608391608392,
+                    "size": 1,
+                    "duration": 3,
+                    "opacity": 1,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+        "interactive": true
+    });
 
     
         addHideNavbarHandlerToTogglerMenu();
